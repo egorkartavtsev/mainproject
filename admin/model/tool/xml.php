d2e85b901df82dd4390f6b6d712cb6cac6df5dbb
egorkartavtsev <egorkartavtsev@gmail.com>
2018-02-06 15:48:14 +0500

    Внесены правки в обновление и создание объявлений Авито

--- admin/model/tool/xml.php
+++ admin/model/tool/xml.php
@@ -5,15 +5,25 @@ class ModelToolXml extends Model {
     public function findAd($data) {
         $xmls = simplexml_load_file('../Avito/ads.xml');
         $sup = 0;
+        $this->load->model('common/avito');
+        $settings = $this->model_common_avito->getSetts();
         foreach($xmls->Ad as $ad){
             if(in_array($data['vin'], (array)$ad)){
-                $this->updateAd($data, $sup, $xmls);
-                return 0;
+                if($data['price']>=$settings['price']){
+                    $this->updateAd($data, $sup, $xmls);
+                    return 0;
+                } else {
+                    $dom=dom_import_simplexml($xmls->Ad[$sup]);
+                    $dom->parentNode->removeChild($dom);
+                    return 0;
+                }
             } else{ 
                 ++$sup;
             }
         }
-        $this->createAd($data, $xmls);
+        if($data['price']>=$settings['price']){
+            $this->createAd($data, $xmls);
+        }
     }
     
     public function createAd($data, $xmls) {
@@ -22,10 +32,11 @@ class ModelToolXml extends Model {
         $this->load->model('product/product');
         $settings = $this->model_common_avito->getSetts();
         $stock = $this->db->query("SELECT * FROM ".DB_PREFIX."stocks WHERE name = '".$data['stock']."'");
+        $phone = $data['stock']=='KM'?'+7 (908) 825-52-40':'‎+7 (912) 475-08-70';
+        $weekend = $data['stock']=='KM'?'<strong>СБ, ВС - выходной</strong>':'СБ 11:00-16:00, <strong>ВС - выходной</strong>';
         //-----------------------------------------
         $templ = $this->model_common_avito->getDescTempl();
         $templ = htmlspecialchars_decode($templ);
-//        $desc = '<![CDATA[';
         /************************/
             $templ = str_replace('%podcat%', $data['podcat'], $templ);
             $templ = str_replace('%brand%', $data['brandname'], $templ);
@@ -39,9 +50,8 @@ class ModelToolXml extends Model {
             $templ = str_replace('%compabil%', $data['compability'], $templ);
             $templ = str_replace('%note%', $data['note'], $templ);
             $templ = str_replace('%dopinfo%', $data['dop'], $templ);
+            $templ = str_replace('%weekend%', $weekend, $templ);
         /************************/
-//            $desc.= $templ;
-//        $desc.= ']]>';
         
         //-----------------------------------------
         $ad = $xmls->addChild('Ad');
@@ -59,6 +69,7 @@ class ModelToolXml extends Model {
             }
             $ad->addChild('Region', 'Челябинская область');
             $ad->addChild('City', 'Магнитогорск');
+            $ad->addChild('ContactPhone', $phone);
             $ad->addChild('Category', 'Запчасти и аксессуары');
             $aid = $this->model_common_avito->getPCID($data['podcat']);
             $ad->addChild('TypeId', $aid);
@@ -96,15 +107,44 @@ class ModelToolXml extends Model {
     }
     
     public function updateAd($data, $id, $xmls) {
+        $this->load->model('common/avito');
+        $aid = $this->model_common_avito->getPCID($data['podcat']);
+        $phone = $data['stock']=='KM'?'+7 (908) 825-52-40':'‎+7 (912) 475-08-70';
+        $stock = $this->db->query("SELECT * FROM ".DB_PREFIX."stocks WHERE name = '".$data['stock']."'");
+        $weekend = $data['stock']=='KM'?'<strong>СБ, ВС - выходной</strong>':'СБ 11:00-16:00, <strong>ВС - выходной</strong>';
+        /************************/
+        $templ = $this->model_common_avito->getDescTempl();
+        $templ = htmlspecialchars_decode($templ);
+            $templ = str_replace('%podcat%', $data['podcat'], $templ);
+            $templ = str_replace('%brand%', $data['brandname'], $templ);
+            $templ = str_replace('%modrow%', $data['mrname'], $templ);
+            $templ = str_replace('%trbrand%', $data['trbrand'], $templ);
+            $templ = str_replace('%trmodrow%', $data['trmodrow'], $templ);
+            $templ = str_replace('%stock%', $stock->row['adress'], $templ);
+            $templ = str_replace('%vin%', $data['vin'], $templ);
+            $templ = str_replace('%catn%', $data['catN'], $templ);
+            $templ = str_replace('%condit%', $data['cond'], $templ);
+            $templ = str_replace('%compabil%', $data['compability'], $templ);
+            $templ = str_replace('%note%', $data['note'], $templ);
+            $templ = str_replace('%dopinfo%', $data['dop'], $templ);
+            $templ = str_replace('%weekend%', $weekend, $templ);
+        /************************/
+        
+        $dom=dom_import_simplexml($xmls->Ad[$id]->Description);
+        $dom->parentNode->removeChild($dom);
+        /*--------------------------------*/
+        $desc = $xmls->Ad[$id]->addChild('Description');
+        $node = dom_import_simplexml($desc);
+        $no = $node->ownerDocument;
+        $node->appendChild($no->createCDATASection($templ));
+        /*--------------------------------*/
         $xmls->Ad[$id]->Price = $data['price'];
         $xmls->Ad[$id]->Title = $data['avitoname'];
-        $aid = $this->model_common_avito->getPCID($data['podcat']);
         $xmls->Ad[$id]->TypeId = $aid;
-        $xmls->saveXML('../Avito/ads.xml');
-    }
-    
-    public function removeAd($id, $xmls) {
+        $xmls->Ad[$id]->ContactPhone = $phone;
         
+        //--------------------------------------------
+        $xmls->saveXML('../Avito/ads.xml');
     }
     
 }
