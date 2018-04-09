59bbb7a458a88aafa820428f81e64be96e679888
Картавый <@DESKTOP-7QI66JS.Dlink>
2018-04-09 23:27:59 +0500

    Скрипты для обновы базы.

--- admin/controller/common/createXML.php
+++ admin/controller/common/createXML.php
@@ -2,6 +2,7 @@
 
 class ControllerCommonCreateXML extends Controller {
     public function index() {
-        $sup = $this->db->query("SELECT * FROM ".DB_PREFIX."");
+        
+        exit();
     }
 }
